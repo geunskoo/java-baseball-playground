37ca150686dd0156b883717832786b880d1cdf52
김태근 <oryukdo3@pusan.ac.kr>
2024-06-03 10:39:13 +0900

    feat: 스코어 전달용 DTO

---
+++ src/main/java/GameScore.java
@@ -0,0 +1,18 @@
+public class GameScore {
+
+    private final int strike;
+    private final int ball;
+
+    public GameScore(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
