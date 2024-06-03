5a74df44dba0462e6a0de58f6dd1f5b0166fdd5d
김태근 <oryukdo3@pusan.ac.kr>
2024-06-03 16:45:33 +0900

    feat: 정답볼 전체와 위치만 다르면 BALL이고, 정답볼 전체와 위치와 값 모두 다르면 NOTHING 이다.(prod, test)

---
+++ src/main/java/baseball/Balls.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Balls {
+
+    private static final int MAX_BALL_COUNT = 3;
+    private List<Ball> balls;
+
+    public Balls(List<Integer> balls) {
+        mapBalls(balls);
+    }
+
+    public BallStatus play(Ball ball) {
+        return this.balls.stream()
+                .map(b -> b.play(ball))
+                .filter(BallStatus::isNotNothing)
+                .findFirst()
+                .orElse(BallStatus.NOTHING);
+    }
+
+    private void mapBalls(List<Integer> balls) {
+        this.balls = new ArrayList<>();
+        for (int i = 0; i < MAX_BALL_COUNT; i++) {
+            this.balls.add(new Ball(i+1, new BallNumber(balls.get(i))));
+        }
+    }
+}
---
+++ src/test/java/baseball/BallsTest.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BallsTest {
+
+    @Test
+    @DisplayName("정답볼 전체와 위치만 다르면 BALL 이다.")
+    void Ball_ShouldOnlyEqualBallNumber(){
+        Balls answers = new Balls(Arrays.asList(1, 2, 3));
+        Ball ball = new Ball(2, new BallNumber(3));
+        assertThat(answers.play(ball)).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
+    @DisplayName("정답볼 전체와 위치와 값 모두 다르면 NOTHING 이다.")
+    void Nothing_ShouldNotEqualPositionAndBallNumber(){
+        Balls answers = new Balls(Arrays.asList(1, 2, 3));
+        Ball ball = new Ball(1, new BallNumber(0));
+        assertThat(answers.play(ball)).isEqualTo(BallStatus.NOTHING);
+    }
+}
